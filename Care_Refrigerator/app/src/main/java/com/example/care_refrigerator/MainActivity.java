6089d9f9b072266a7a193e53b3a406b97e3b455d
gibum1228 <gibum1228@naver.com>
2020-04-14 05:07:30 +0900

    2020.04.14.

--- Care_Refrigerator/app/src/main/java/com/example/care_refrigerator/MainActivity.java
+++ Care_Refrigerator/app/src/main/java/com/example/care_refrigerator/MainActivity.java
@@ -9,39 +9,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-//    Button boardBtn = (Button)findViewById(R.id.boardBtn);
-    Button boxBtn = (Button)findViewById(R.id.boxBtn);
-    Button pushBtn = (Button)findViewById(R.id.pushBtn);
-    Button optionBtn = (Button)findViewById(R.id.optionBtn);
+    Button optionBtn;
+    Button boardBtn;
+    Button boxBtn;
+    Button pushBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 액티비티 전환
+        boardBtn = (Button)findViewById(R.id.boardBtn);
+        pushBtn = (Button)findViewById(R.id.pushBtn);
+        boxBtn = (Button)findViewById(R.id.boxBtn);
+        optionBtn = (Button)findViewById(R.id.optionBtn);
+
         optionBtn.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               Intent intent = new Intent(getApplicationContext(), OptionActivity.class);
+                startActivity(intent);
+           }
+        });
+
+        boardBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, OptionActivity.class);
+                Intent intent = new Intent(getApplicationContext(), BoardActivity.class);
                 startActivity(intent);
             }
         });
-        pushBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, PushActivity.class);
-                startActivity(intent);
-            }
-        });
+
         boxBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, BoxActivity.class);
+                Intent intent = new Intent(getApplicationContext(), BoxActivity.class);
                 startActivity(intent);
             }
         });
-    }
 
+        pushBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), PushActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 
 }
